11a775f93a99a041d49f31baa490d4bbe718b4d3
NikoZ21 <89398005+NikoZ21@users.noreply.github.com>
2025-01-15 19:27:28 +0400

    finsihed making board and player turns

--- Tic-Tac-Toe/src/App.jsx
+++ Tic-Tac-Toe/src/App.jsx
@@ -1,14 +1,47 @@
 import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
+import { useState } from "react";
 
 function App() {
+  const [gameTurns, setGameTurns] = useState([]);
+  const [activePlayer, setActivePlayer] = useState("X");
+
+  function handleSquareSelected(rowIndex, colIndex) {
+    setActivePlayer((prevValue) => (prevValue === "X" ? "O" : "X"));
+    setGameTurns((prevTurns) => {
+      let currentPlayer = "X";
+
+      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
+        currentPlayer = "O";
+      }
+      const updatedTurns = [
+        {
+          square: { row: rowIndex, col: colIndex },
+          player: currentPlayer,
+        },
+        ...prevTurns,
+      ];
+
+      return updatedTurns;
+    });
+  }
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player name="Player 1" symbol="X" />
-          <Player name="Player 2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
         </ol>
-        GAME BOARD
+        <GameBoard onSquareSelected={handleSquareSelected} turns={gameTurns} />
       </div>
       LOG
     </main>
--- Tic-Tac-Toe/src/components/Player.jsx
+++ Tic-Tac-Toe/src/components/Player.jsx
@@ -1,21 +1,34 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol, isActive }) {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState();
 
   function handleClick() {
     setIsEditing((isEditing) => !isEditing);
   }
 
-  let playerName = <span className="player-name">{name}</span>;
+  function handleNameChanged(event) {
+    console.log(event);
+    setPlayerName(event.target.value);
+  }
+
+  let playerNameHTML = <span className="player-name">{playerName}</span>;
   if (isEditing) {
-    playerName = <input type="text" required value={name} />;
+    playerNameHTML = (
+      <input
+        type="text"
+        required
+        value={playerName}
+        onChange={handleNameChanged}
+      />
+    );
   }
 
   return (
-    <li>
+    <li className={isActive ? "active" : undefined}>
       <span className="player">
-        {playerName}
+        {playerNameHTML}
         <span className="player-symbol">{symbol}</span>
       </span>
       <button onClick={handleClick}>{isEditing ? "Save" : "Edit"}</button>
