12ab807a4e159b7c2a849792c73fafce31c22b90
NikoZ21 <89398005+NikoZ21@users.noreply.github.com>
2025-01-22 18:11:55 +0400

    added sidebar

---
+++ React-Project-Management/src/components/Input.jsx
@@ -0,0 +1,3 @@
+export default function Input() {
+  return <input className="w-64 px-2 py-1 rounded-sm bg-stone-200" />;
+}
---
+++ React-Project-Management/src/components/NewProject.jsx
@@ -0,0 +1,22 @@
+import Input from "./Input.jsx";
+export default function NewProject() {
+  return (
+    <div className="mt-24 text-center w-2/3">
+      <menu className="flex items-center justify-end gap-4 my-4">
+        <li>
+          <button className="text-stone-800 hover:text-stone-950">
+            Cancel
+          </button>
+        </li>
+        <li>
+          <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">
+            Save
+          </button>
+        </li>
+      </menu>
+      <div>
+        <Input />
+      </div>
+    </div>
+  );
+}
