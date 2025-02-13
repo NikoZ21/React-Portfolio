2fe65c5ad75c39e6aa0a30222c2db816aedb5cb9
NikoZ21 <89398005+NikoZ21@users.noreply.github.com>
2025-02-13 23:25:06 +0400

    ...

--- PlacePicker/src/App.jsx
+++ PlacePicker/src/App.jsx
@@ -1,15 +1,10 @@
-<<<<<<< Updated upstream
 import { useRef, useState, useEffect } from "react";
 
-=======
-import { useRef, useState } from "react";
->>>>>>> Stashed changes
 import Places from "./components/Places.jsx";
 import { AVAILABLE_PLACES } from "./data.js";
 import Modal from "./components/Modal.jsx";
 import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
 import logoImg from "./assets/logo.png";
-<<<<<<< Updated upstream
 import { sortPlacesByDistance } from "./loc.js";
 
 const key = "selectedPlaces";
@@ -17,8 +12,6 @@ const storeIds = JSON.parse(localStorage.getItem(key)) || [];
 const storedPlaces = storeIds.map((id) =>
   AVAILABLE_PLACES.find((place) => place.id === id)
 );
-=======
->>>>>>> Stashed changes
 
 function App() {
   const modal = useRef();
